ceadefaddbfaa544d318da7be21207807740123e
Christian Arana <coanelmejor@gmail.com>
2019-09-19 00:47:06 -0600

    Beautify to the app.js file

--- app.js
+++ app.js
@@ -145,22 +145,22 @@ app.get('/profile',
   function (req, res) {
     res.render('profile', {
       user: req.user,
-       title: 'My Profile' 
+      title: 'My Profile'
     });
   });
 
-  
+
 // URLs to serve
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
